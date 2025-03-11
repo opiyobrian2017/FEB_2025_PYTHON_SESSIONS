97a6943faa88c52c4900706775507155ee8fea9e
opiyobrian2017 <opiyobrian2017@gmail.com>
2025-03-11 11:41:31 +0300

    sample data types

--- Week 1/index.py
+++ Week 1/index.py
@@ -1,3 +1,19 @@
 print("Hello world")
 number = 10
 print(number)
+
+# Datatypes
+
+age = 27 #intager data type
+marks = 90.5 # float data types
+name = "Jane Doe" # string data types
+
+print("My name is", name, "I am", age, "years old and I got", marks, "marks in the exam")
+
+# List datatypes
+
+Languages = ["Python", "HTML", "CSS", 3, 6.57, True]
+print(Languages) #printing an entire list
+print(Languages[0]) #printing a single item from the list using index position
+print(Languages[0:2]) # printing a number of items frim a list
+print(Languages[-1]) # printing using negative indices
