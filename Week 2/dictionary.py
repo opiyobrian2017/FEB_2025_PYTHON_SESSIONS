8a04bff6bb7bc7a41b708ff1ccf0aaefb9782e47
opiyobrian2017 <opiyobrian2017@gmail.com>
2025-03-18 11:02:17 +0300

    Adding lists and dictionary

---
+++ Week 2/dictionary.py
@@ -0,0 +1,10 @@
+capital_city = {
+    "India": "New Delhi", "Nairobi": "Kenya", "London": "England", "Washington": "USA", "Paris": "France"
+    }
+print(capital_city)
+print(capital_city["India"])
+
+## append value to dictionary
+capital_city["Tokyo"] = "Japan"
+print(capital_city)
+print(capital_city["Tokyo"])
